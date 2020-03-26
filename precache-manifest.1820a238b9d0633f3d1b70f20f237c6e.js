b073c6571042c44473ff7a7a54c312e0697095e7
Stas Fedorko <stasfedorko@MacBook-Pro-Stas.local>
2020-03-26 22:42:38 +0200

    Updates

---
+++ precache-manifest.1820a238b9d0633f3d1b70f20f237c6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c184ddee0e67adf5408b8f52d7553764",
+    "url": "/hw15r/index.html"
+  },
+  {
+    "revision": "475c61317813c027df69",
+    "url": "/hw15r/static/css/main.4124717e.chunk.css"
+  },
+  {
+    "revision": "db0dc790753adb8e1caf",
+    "url": "/hw15r/static/js/2.8b93e603.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hw15r/static/js/2.8b93e603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "475c61317813c027df69",
+    "url": "/hw15r/static/js/main.90c746df.chunk.js"
+  },
+  {
+    "revision": "3de0f8ca97dc840ae6d2",
+    "url": "/hw15r/static/js/runtime-main.1d9c5c58.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hw15r/static/media/logo.5d5d9eef.svg"
+  }
+]);
